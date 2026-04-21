86bc1532242b4ba0b0fee0067a585d3a347da447
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-04-21 15:20:53 +0530

    Add project files.

---
+++ StateManagement/ViewState.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewState.aspx.cs" Inherits="StateManagement.ViewState" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server"  ></asp:Label>            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />            <%--<asp:CustomValidator ID="CustomValidator1" runat="server" ErrorMessage="CustomValidator" OnServerValidate="CustomValidator1_ServerValidate" ></asp:CustomValidator>--%>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StateManagement/ViewState.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManagement
+{
+    public partial class ViewState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["Clickscount"] = 0;
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int count = (int)ViewState["Clickscount"];
+            count++;
+            ViewState["Clickscount"] = count;
+            Label1.Text = "Click count" + count;
+
+        }
+    }
+}
