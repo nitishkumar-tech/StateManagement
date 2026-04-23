466ca615c1c4b9ab3cc46e84d63d8429b49e537b
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-04-23 10:57:18 +0530

    new page has been pushed

---
+++ StateManagement/NewSession-1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewSession-1.aspx.cs" Inherits="StateManagement.NewSession_1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Session Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Text="Click Button"></asp:Label>
+            <br /><br />
+
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblCount" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ StateManagement/NewSession-1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StateManagement  
+{
+    public partial class NewSession_1 : System.Web.UI.Page   
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["ClicksCount"] == null)
+            {
+                Session["ClicksCount"] = 0;
+            }
+
+            lblCount.Text = "Clicks count: " + Session["ClicksCount"].ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int count = (int)Session["ClicksCount"];
+            count++;
+
+            Session["ClicksCount"] = count;
+
+            lblCount.Text = "Clicks count: " + Session["ClicksCount"].ToString();
+        }
+    }
+}
