60b53780ff15edad04c3897be88a39fc1ef69aea
NITISH KUMAR <NITISH KUMAR@LAPTOP-95P3EDGR>
2026-04-23 12:34:01 +0530

    new line is entered#

---
+++ StateManagement/NewSession-2.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewSession-2.aspx.cs" Inherits="StateManagement.NewSession_2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblUserCount" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StateManagement/NewSession-2.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManagement
+{
+    public partial class NewSession_2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int CurrentUserCount = (int)Application["userCount"];
+            lblUserCount.Text = "Current User Count: " + CurrentUserCount.ToString();
+        }
+    }
+}
